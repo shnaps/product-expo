9d07191f787af69b0f5f619b09c1900d9d2fb470
shnaps <shnapsx@gmail.com>
2019-05-10 15:39:55 -0600

    Move repositories in repo package

--- src/main/java/me/shnaps/productexpo/repository/PaymentRepository.java
+++ src/main/java/me/shnaps/productexpo/repository/PaymentRepository.java
@@ -1,7 +1,10 @@
-package me.shnaps.productexpo.entity.payment;
+package me.shnaps.productexpo.repository;
 
+import me.shnaps.productexpo.entity.payment.Payment;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("paymentRepository")
 public interface PaymentRepository extends PagingAndSortingRepository<Payment, Long> {
     Payment getPaymentByCardNumber(String cardNumber);
 }
--- src/main/java/me/shnaps/productexpo/repository/UserRepository.java
+++ src/main/java/me/shnaps/productexpo/repository/UserRepository.java
@@ -1,7 +1,10 @@
-package me.shnaps.productexpo.entity.user;
+package me.shnaps.productexpo.repository;
 
+import me.shnaps.productexpo.entity.user.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("userRepository")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     User getUserByEmail(String email);
 }
