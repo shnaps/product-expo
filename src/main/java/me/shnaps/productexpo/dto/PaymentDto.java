1c605671af559137dd56c592b5b62687a2128e50
shnaps <shnapsx@gmail.com>
2019-05-10 17:12:31 -0600

    Fix DTOs for jsonMapper

--- src/main/java/me/shnaps/productexpo/dto/PaymentDto.java
+++ src/main/java/me/shnaps/productexpo/dto/PaymentDto.java
@@ -2,9 +2,7 @@ package me.shnaps.productexpo.dto;
 
 import me.shnaps.productexpo.entity.Payment;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 public class PaymentDto {
     private String cardNumber;
@@ -12,10 +10,45 @@ public class PaymentDto {
     private Integer cvv;
     private String paymentAddress;
 
+    public PaymentDto() {
+    }
+
     public PaymentDto(String cardNumber, LocalDateTime expireDate, Integer cvv, String paymentAddress) {
         this.cardNumber = cardNumber;
         this.expireDate = expireDate;
         this.cvv = cvv;
+        this.paymentAddress = paymentAddress;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public LocalDateTime getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(LocalDateTime expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public Integer getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(Integer cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getPaymentAddress() {
+        return paymentAddress;
+    }
+
+    public void setPaymentAddress(String paymentAddress) {
         this.paymentAddress = paymentAddress;
     }
 
