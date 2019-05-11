3e7e2101b866f4491380541ee840efb4f7a55a40
shnaps <shnapsx@gmail.com>
2019-05-11 16:02:55 -0600

    Use constructors for services instead of @Autowired

--- src/main/java/me/shnaps/productexpo/service/implementation/PaymentServiceImpl.java
+++ src/main/java/me/shnaps/productexpo/service/implementation/PaymentServiceImpl.java
@@ -3,13 +3,15 @@ package me.shnaps.productexpo.service.implementation;
 import me.shnaps.productexpo.entity.Payment;
 import me.shnaps.productexpo.repository.PaymentRepository;
 import me.shnaps.productexpo.service.PaymentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
-    @Autowired
-    private PaymentRepository repository;
+    private final PaymentRepository repository;
+
+    public PaymentServiceImpl(PaymentRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public Payment save(Payment payment) {
--- src/main/java/me/shnaps/productexpo/service/implementation/UserServiceImpl.java
+++ src/main/java/me/shnaps/productexpo/service/implementation/UserServiceImpl.java
@@ -3,13 +3,15 @@ package me.shnaps.productexpo.service.implementation;
 import me.shnaps.productexpo.entity.User;
 import me.shnaps.productexpo.repository.UserRepository;
 import me.shnaps.productexpo.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserRepository repository;
+    private final UserRepository repository;
+
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public User save(User user) {
