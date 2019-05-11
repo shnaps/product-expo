0679eb940fdc11281d712949981acb955c7c1bbb
shnaps <shnapsx@gmail.com>
2019-05-11 16:01:18 -0600

    Create OrchestrationController

---
+++ src/main/java/me/shnaps/productexpo/controller/OrchestrationController.java
@@ -0,0 +1,22 @@
+package me.shnaps.productexpo.controller;
+
+import me.shnaps.productexpo.dto.ResultDto;
+import me.shnaps.productexpo.service.OrchestrationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrchestrationController {
+
+    private final OrchestrationService service;
+
+    public OrchestrationController(OrchestrationService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/proceed")
+    public ResultDto proceedCreation() {
+        return service.proceedCreate(UserController.finalUser, PaymentController.finalPayment);
+    }
+}
