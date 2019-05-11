c5a4984f2b41bfe27c3823b1afa739a5fc6f18b7
shnaps <shnapsx@gmail.com>
2019-05-11 16:03:55 -0600

    Create response DTO-object for OrchestrationController

---
+++ src/main/java/me/shnaps/productexpo/dto/ResultDto.java
@@ -0,0 +1,29 @@
+package me.shnaps.productexpo.dto;
+
+import me.shnaps.productexpo.entity.User;
+
+public class ResultDto {
+    private PaymentDto paymentDto;
+    private UserDto userDto;
+
+    public ResultDto(PaymentDto paymentDto, UserDto userDto) {
+        this.paymentDto = paymentDto;
+        this.userDto = userDto;
+    }
+
+    public PaymentDto getPaymentDto() {
+        return paymentDto;
+    }
+
+    public void setPaymentDto(PaymentDto paymentDto) {
+        this.paymentDto = paymentDto;
+    }
+
+    public UserDto getUserDto() {
+        return userDto;
+    }
+
+    public void setUserDto(UserDto userDto) {
+        this.userDto = userDto;
+    }
+}
